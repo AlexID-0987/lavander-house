15f93a0cc60d5725b80b862c40a07ac663ea1e6c
Alex <a1980dominov@gmail.com>
2022-12-20 06:06:54 +0200

    Added sessions

--- lavanders/shared/urls.py
+++ lavanders/shared/urls.py
@@ -3,5 +3,6 @@ from . import views
 #first page
 urlpatterns=[ 
     path('',views.index,name='beginen'),
-    path('<int:prod_id>', views.order, name='order')
+    path('<int:prod_id>', views.order, name='order'),
+    path('buy/<int:buy_id>/', views.buy, name='buy')
 ]
--- lavanders/shared/views.py
+++ lavanders/shared/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from .models import Products
+from django import forms
+
+class NewForms(forms.Form):
+    name=forms.CharField(label='Name')
+    email=forms.CharField(label='Email')
+    
 
 # Create your views here.
 def index(request):
@@ -10,5 +16,27 @@ def index(request):
 def order(request, prod_id):
     order_prod=Products.objects.get(pk=prod_id)
     return render(request, 'shared/order.html',{
-        'order':order_prod
+        'order':order_prod,
+    })
+
+def buy(request, buy_id):
+    ses=None
+    quantity=1
+    if 'order_products' in request.session:
+        if buy_id in request.session['order_products']:
+            request.session['order_products'].remove(buy_id)
+        request.session['order_products'].insert(0, buy_id)
+        ses=Products.objects.filter(pk__in=request.session['order_products'])
+        request.session.set_expiry(600)
+    else:
+        request.session['order_products']=[buy_id]
+        
+    prod=Products.objects.get(pk=buy_id)
+    request.session.modified=True
+    return render(request, 'shared/buy.html',{
+        'find':ses,
+        'buy_id':buy_id,
+        'buy_form':NewForms(),
+        'prod':prod,
+        'quantity':quantity
     })
